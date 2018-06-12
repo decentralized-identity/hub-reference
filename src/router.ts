53ea2d1ba26f82720909d1bfd3331b06c3e69f04
Henry Tsai <hentsai@microsoft.com>
2018-06-12 00:15:39 -0700

    Updated code to use Hub object in hub-node-core.

--- src/router.ts
+++ src/router.ts
@@ -1,16 +1,16 @@
 import * as Router from 'koa-router';
-import * as HubCore from 'hub-node-core';
+import Hub from 'hub-node-core';
 import { InMemoryStore } from 'hub-node-core';
 
 // NOTE: substitute with real store module when available.
-// For now using a test in-memory store module.
+// For now assuming a test in-memory store module in hub-node-core to illustrate the architecture.
 const store = InMemoryStore.get();
-HubCore.initialize({store: store});
+const hub = new Hub({store: store});
 
 const router = new Router();
 router.post('*', async ctx => {
   const hubKeyId = ctx.headers['hub-key-id'];
-  const apiResponse = await HubCore.handleRequest(ctx.body, { hubKeyId: hubKeyId });
+  const apiResponse = await hub.handleRequest(ctx.body, { hubKeyId: hubKeyId });
 
   ctx.response.body = apiResponse.getResponseBody();
   ctx.response.status = apiResponse.getResponseCode();
